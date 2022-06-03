edfab2344000e82b44a3c8a2995287b6061aae66
ZeraiGR <kirillgr15@mail.ru>
2022-06-03 23:25:09 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+			<header className="header">
+					<nav className="header__nav">
+						<a className="logo" href="#">
+							<img src="/img/logo.png" alt="logo" />
+							
+							<div className="logo__title">
+								<strong>REACT SNEAKERS</strong>
+								<span>Магазин лучших кроссовок</span>
+							</div>
+						</a>
+						<ul className="header__menu">
+							<li>
+								<button className="header__cart" type="button">
+									
+									1205 руб.
+							 	</button>
+							</li>
+							 <li>
+									<a className="header__link header__link--favorite" href="#">
+										<span className="sr-only">Избранное</span>
+									</a>
+							 </li>
+							 <li>
+									<a className="header__link header__link--user" href="#">
+										<span className="sr-only">Профиль</span>
+									</a>
+							 </li>
+						</ul>
+					</nav>
+			</header>
+
+			<section className="promo">
+				<div className="promo__container">
+
+				</div>
+			</section>
     </div>
   );
 }
